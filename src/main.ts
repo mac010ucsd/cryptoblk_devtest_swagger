b43acd44a3643cef0fe31ba6a202a9e181fefbd2
Matthew Chan <mchan11101@AIR.hk.office.cryptoblk.io>
2022-07-13 15:23:53 +0800

    express

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
+const serverless = require('serverless-http');
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -13,6 +15,9 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
 	
-	await app.listen(80);
+	//await app.listen(80);
+	const app2 = express();
+	await app.init();
+	module.exports.handler = serverless(app2);
 }
 bootstrap();
